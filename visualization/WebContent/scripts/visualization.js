d09f9850a2c132bd3e3f3df407c70f9059c40878
Felipe Quezada <kezada66@gmail.com>
2017-04-22 16:51:40 -0300

    Primer prototipo visualizacion final

--- visualization/WebContent/scripts/visualization.js
+++ visualization/WebContent/scripts/visualization.js
@@ -1,128 +1,39 @@
-/*******************************
- *       Author: Tom May       *
- *  Modified by:Felipe Quezada *
- *             2017            *
- *******************************/
-
-
 function main(){
-    var margin = { top: 50, right: 0, bottom: 100, left: 30 },
-          width = 960 - margin.left - margin.right,
-          height = 430 - margin.top - margin.bottom,
-          gridSize = Math.floor(width / 24),
-          legendElementWidth = gridSize*2,
-          buckets = 9,
-          colors = ["#4d004d","#660066","#800080","#990099","#b300b3","#cc00cc","#e600e6","#ff00ff","#ff1aff"], // alternatively colorbrewer.YlGnBu[9]
-          days = ["P", "Q", "E", "A-E", "-", "-", "-"],
-          times = ["T1a", "T2a", "T3a", "T4a", "T5a", "T6a", "T7a", "T8a", "T9a", "T10a", "T11a", "T12a", "T13p", "T14p", "T15p", "T16p", "T17p", "T18p", "T19p", "T20", "T21p", "T22p", "T23", "T24p"];
-          datasets = ["test1.tsv", "test2.tsv"];
+	var topicNames = ["Topic 1", "Topic 2", "Topic 3", "Topic 4", "Topic 5", "Topic 6", "Topic 7", "Topic 8"];
+	var headerNames = ["Tópico", "Quizpet", "Parsons", "Animated Ex", "Annotated Ex"];
+	var tiempo    = [6,5,4,3,4,5,6,2,5,4,3,4,5,3,4,5,3,2,4,5,4,3,4,5,3,3,5,3,4,3,4,4];
+	var colors = ["#00ff00", "#ffff00", "#ff0000"];
+	var dificultad= [1,3,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,2,3,1,2,3,1,3,2,2,1,2,3,3];
+	
+	var margins = {top : 100, left : 50, bottom : 0, right : 50};
+	var padding = {top : 30, left : 50, bottom : 30, right : 50};
+	
+	var timeScale = d3.scaleLinear().domain([2,6]).range([4,15]);
 
-      var svg = d3.select("#chart").append("svg")
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-          .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	var width = 1000;
+	var minItemHeight = 10;
+	var svg = d3.select("body").append("svg")
+		.attr("width", width + margins.left + margins.right)
+		.attr("height", 600 + margins.top + margins.bottom);
+	
+	var headers = svg.selectAll(".headers").data(headerNames).enter().append("text")
+		.text(function(d){return d;})
+		.attr("class", "headers")
+		.attr("x", function(d,i){return (i*120) + padding.left})
+		.attr("y", padding.top);
+	
+	var topics = svg.selectAll(".topics").data(topicNames).enter().append("text")
+		.text(function(d){return d;})
+		.attr("class", "topics")
+		.attr("x", padding.left )
+		.attr("y", function(d,i){return ( (i+2)*(padding.top+minItemHeight));});
+	
+	var circles = svg.selectAll("circles").data(tiempo).enter().append("circle");
+	
+	circles.attr("cx", function(d,i){return ((i%4+1)*120 + padding.left*1.5);})
+		.attr("cy", function(d,i){return ( 40 )*Math.floor(i/4)+padding.top*2.6;})
+		.attr("r", function(d,i){return timeScale(d)})
+		.attr("fill", function(d,i){return colors[dificultad[i]-1];});
 
-      var dayLabels = svg.selectAll(".dayLabel")
-          .data(days)
-          .enter().append("text")
-            .text(function (d) { return d; })
-            .attr("x", 0)
-            .attr("y", function (d, i) { return i * gridSize; })
-            .style("text-anchor", "end")
-            .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-            .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
-
-      var timeLabels = svg.selectAll(".timeLabel")
-          .data(times)
-          .enter().append("text")
-            .text(function(d) { return d; })
-            .attr("x", function(d, i) { return i * gridSize; })
-            .attr("y", 0)
-            .style("text-anchor", "middle")
-            .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-            .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
-
-      var heatmapChart = function(tsvFile) {
-        d3.tsv(tsvFile,
-        function(d) {
-          return {
-            day: +d.day,
-            hour: +d.hour,
-            value: +d.value
-            
-          };
-          
-        },
-        function(error, data) {
-          var colorScale = d3.scaleQuantile()
-              .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
-              .range(colors);
-
-          var cards = svg.selectAll(".hour")
-              .data(data, function(d) {return d.day+':'+d.hour;});
-
-          cards.append("title");
-
-          cards.enter().append("rect")
-              .attr("x", function(d) { return (d.hour - 1) * gridSize; })
-              .attr("y", function(d) { return (d.day - 1) * gridSize; })
-              .attr("rx", 4)
-              .attr("ry", 4)
-              .attr("class", "hour bordered")
-              .attr("width", gridSize)
-              .attr("height", gridSize)
-              .style("fill", colors[0]);
-
-          cards.transition().duration(1000)
-              .style("fill", function(d) { return colorScale(d.value); });
-
-          cards.select("title").text(function(d) { return d.value; });
-          
-          cards.exit().remove();
-
-          var legend = svg.selectAll(".legend")
-              .data([0].concat(colorScale.quantiles()), function(d) { return d; });
-
-          legend.enter().append("g")
-              .attr("class", "legend");
-
-          legend.append("rect")
-            .attr("x", function(d, i) { return legendElementWidth * i; })
-            .attr("y", height)
-            .attr("width", legendElementWidth)
-            .attr("height", gridSize / 2)
-            .style("fill", function(d, i) { return colors[i]; });
-
-          legend.append("text")
-            .attr("class", "mono")
-            .text(function(d) { return "≥ " + Math.round(d); })
-            .attr("x", function(d, i) { return legendElementWidth * i; })
-            .attr("y", height + gridSize);
-
-          legend.exit().remove();
-
-        });  
-      };
-
-      heatmapChart(datasets[0]);
-      
-      var datasetpicker = d3.select("#dataset-picker").selectAll(".dataset-button")
-        .data(datasets);
-
-      datasetpicker.enter()
-        .append("input")
-        .attr("value", function(d){ return "Dataset " + d })
-        .attr("type", "button")
-        .attr("class", "dataset-button")
-        .on("click", function(d) {
-          heatmapChart(d);
-        });
-        
-        //Seleccion por defecto: dataset1.
-        heatmapChart(datasets[0]);  
+	
 }
-
-$(window).ready(function() {
-    main();
-});
